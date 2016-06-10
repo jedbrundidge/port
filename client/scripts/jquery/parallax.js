93829c7d65184c3f6033fca17ea37b0db9d538d4
jedbrun@hotmail.com <jedbrun@hotmail.com>
2016-06-10 00:07:02 -0500

    Thursday 6/9/2016 12:06 Added all scss with transitions and transforms with basic layout complete

--- client/scripts/jquery/parallax.js
+++ client/scripts/jquery/parallax.js
@@ -1,3 +1,10 @@
 /**
  * Created by jed on 6/9/2016.
  */
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 1000);
+    return false;
+});
